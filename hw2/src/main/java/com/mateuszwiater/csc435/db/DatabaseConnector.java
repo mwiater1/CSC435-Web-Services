c5b8f0e882c4b3be714a09c8a5a5c2382ede2a17
Mateusz Wiater <mateuszwiater@gmail.com>
2017-03-23 02:21:35 -0400

    Reworked connector to consume all exceptions.

--- hw2/src/main/java/com/mateuszwiater/csc435/db/DatabaseConnector.java
+++ hw2/src/main/java/com/mateuszwiater/csc435/db/DatabaseConnector.java
@@ -1,10 +1,14 @@
 package com.mateuszwiater.csc435.db;
 
+import com.mateuszwiater.csc435.model.ModelResponse;
+import com.mateuszwiater.csc435.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 public class DatabaseConnector {
@@ -14,13 +18,9 @@ public class DatabaseConnector {
 
     public static void main(String[] args) {
         bootstrap(getConnection());
-        Scanner s = new Scanner(System.in);
-        String in = s.nextLine();
-        while(!in.equals("quit")) {
 
-
-            in = s.nextLine();
-        }
+        Locale l = new Locale("", "au");
+        System.out.println("Country: " + l.getDisplayCountry());
     }
 
     private static void bootstrap(final Connection con) {
@@ -34,10 +34,23 @@ public class DatabaseConnector {
                 System.out.print("Bootstrapping...");
                 Statement st = con.createStatement();
                 st.addBatch("CREATE TABLE USERS (" +
-                        "ID int NOT NULL AUTO_INCREMENT," +
+//                        "ID int NOT NULL AUTO_INCREMENT," +
                         "USERNAME VARCHAR(255) NOT NULL," +
                         "PASSWORD VARCHAR(255) NOT NULL," +
                         "APIKEY UUID NOT NULL," +
+                        "PRIMARY KEY (USERNAME));");
+
+                // Create the Sources table
+                st.addBatch("CREATE TABLE SOURCES (" +
+                        "ID VARCHAR(255) NOT NULL," +
+                        "NAME VARCHAR(255) NOT NULL," +
+                        "DESCRIPTION VARCHAR(255) NOT NULL," +
+                        "URL VARCHAR(255) NOT NULL," +
+                        "CATEGORY VARCHAR(255) NOT NULL," +
+                        "LANGUAGE VARCHAR(255) NOT NULL," +
+                        "COUNTRY VARCHAR(255) NOT NULL," +
+                        "LOGOS VARCHAR(255) NOT NULL," +
+                        "SORTBYS VARCHAR(255) NOT NULL," +
                         "PRIMARY KEY (ID));");
 
                 // Add dummy users
@@ -73,10 +86,42 @@ public class DatabaseConnector {
         }
     }
 
-    public static Connection getConnection() {
+    private static Connection getConnection() {
         if (instance == null) {
             instance = new DatabaseConnector();
         }
         return instance.connection;
+    }
+
+    public static SqlResponse runQuery(final String query) {
+        List<List<String>> table = new ArrayList<>();
+
+        try (final Statement stmt = getConnection().createStatement()) {
+            // Check if there is a ResultSet
+            if (stmt.execute(query)) {
+                // Convert the ResultSet to List<List<String>>
+                try (final ResultSet rs = stmt.getResultSet()) {
+                    while (rs.next()) {
+                        final List<String> row = new ArrayList<>();
+                        for(int i = 1; i < rs.getMetaData().getColumnCount() + 1; i++) {
+                            row.add(rs.getString(i));
+                        }
+                        table.add(row);
+                    }
+
+                    return new SqlResponse(SqlStatus.OK, table.size() == 0 ? null : table, "");
+                }
+            } else {
+                return new SqlResponse(SqlStatus.OK, null, "");
+            }
+        } catch (SQLException e) {
+            // Check for Primary Key Violation
+            if(e.getSQLState().equals("23505")) {
+                return new SqlResponse(SqlStatus.PRIMARY_KEY_VIOLATION, null, "");
+            } else {
+                logger.error("SQL Error", e);
+                return new SqlResponse(SqlStatus.SQL_ERROR, null, "");
+            }
+        }
     }
 }
