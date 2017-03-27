b825d10a9979e93d01577db2f641410bb06ab1fd
Mateusz Wiater <mateuszwiater@gmail.com>
2017-03-26 21:43:43 -0400

    Finished Source model and testing.

--- hw2/src/main/java/com/mateuszwiater/csc435/model/Source.java
+++ hw2/src/main/java/com/mateuszwiater/csc435/model/Source.java
@@ -1,30 +1,44 @@
 package com.mateuszwiater.csc435.model;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.mateuszwiater.csc435.db.DatabaseConnector;
+
+import java.lang.reflect.Type;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Source {
+    private boolean isNew;
     private List<String> sortBysAvailable;
     private Map<String, String> urlsToLogos;
     private String id, name, description, url, category, language, country;
 
-    public static ModelResponse<List<Source>> getSources(final Category category, final Language language, final Country country) {
-        String query = "SELECT * FROM SOURCES";
-
-        return null;
-    }
-
     public Source(String id, String name, String description, String url, String category, String language,
-                  String country, Map<String, String> urlsToLogos, List<String> sortBysAvailable) {
+                   String country, Map<String, String> urlsToLogos, List<String> sortBysAvailable) {
         this.id = id;
         this.url = url;
         this.name = name;
+        this.isNew = true;
         this.country = country;
         this.language = language;
         this.category = category;
         this.description = description;
         this.urlsToLogos = urlsToLogos;
         this.sortBysAvailable = sortBysAvailable;
+    }
+
+    public void save() throws SQLException {
+        if (isNew) {
+            final Gson gson = new Gson();
+            final String query = String.format("INSERT INTO SOURCES (ID,URL,NAME,COUNTRY,LANGUAGE,CATEGORY,DESCRIPTION,LOGOS,SORTBYS) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s');",
+                    id, url, name, country, language, category, description, gson.toJson(urlsToLogos), gson.toJson(sortBysAvailable));
+            DatabaseConnector.runQuery(query);
+        }
     }
 
     public String getId() {
@@ -61,5 +75,55 @@ public class Source {
 
     public List<String> getSortBysAvailable() {
         return sortBysAvailable;
+    }
+
+    public static Optional<Source> getSource(final String id) throws SQLException {
+        final String query = String.format("SELECT * FROM SOURCES WHERE ID = '%s'", id);
+
+        final Optional<List<List<String>>> res = DatabaseConnector.runQuery(query);
+
+        if(res.isPresent()) {
+            return Optional.of(toSource(res.get().get(0)));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static List<Source> getSources(final Category category, final Language language, final Country country) throws SQLException {
+        String query = "";
+
+        if (category != null) {
+            query += String.format(" AND CATEGORY = '%s'", category.getName());
+        }
+
+        if (language != null) {
+            query += String.format(" AND LANGUAGE = '%s'", language.getCode());
+        }
+
+        if (country != null) {
+            query += String.format(" AND COUNTRY = '%s'", country.getCode());
+        }
+
+        query = "SELECT * FROM SOURCES" + query.replaceFirst("AND","WHERE") + ";";
+
+        final Optional<List<List<String>>> res = DatabaseConnector.runQuery(query);
+
+        if(res.isPresent()) {
+            return res.get().stream()
+                    .map(Source::toSource)
+                    .collect(Collectors.toList());
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    private static Source toSource(final List<String> l) {
+        final Gson gson = new Gson();
+        final Type mapType = new TypeToken<Map<String, String>>() {}.getType();
+        final Type listType = new TypeToken<List<String>>() {}.getType();
+        final Source source = new Source(l.get(0), l.get(1), l.get(2), l.get(3), l.get(4), l.get(5), l.get(6), gson.fromJson(l.get(7), mapType), gson.fromJson(l.get(8), listType));
+        // Make sure this record is not being saved.
+        source.isNew = false;
+        return source;
     }
 }
---
+++ hw2/src/test/java/com/mateuszwiater/csc435/model/SourceTest.java
@@ -0,0 +1,186 @@
+package com.mateuszwiater.csc435.model;
+
+import com.google.gson.Gson;
+import com.mateuszwiater.csc435.db.DatabaseConnector;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class SourceTest {
+    @Before
+    public void before() throws SQLException {
+        // Create the sources table
+        DatabaseConnector.runQuery("CREATE TABLE SOURCES (" +
+                "ID VARCHAR(255) NOT NULL," +
+                "NAME VARCHAR(255) NOT NULL," +
+                "DESCRIPTION VARCHAR(255) NOT NULL," +
+                "URL VARCHAR(255) NOT NULL," +
+                "CATEGORY VARCHAR(255) NOT NULL," +
+                "LANGUAGE VARCHAR(255) NOT NULL," +
+                "COUNTRY VARCHAR(255) NOT NULL," +
+                "LOGOS VARCHAR(255) NOT NULL," +
+                "SORTBYS VARCHAR(255) NOT NULL," +
+                "PRIMARY KEY (ID));");
+
+        // Create the test sources
+        Gson g = new Gson();
+        final Map<String, String> urlsToLogos = new HashMap<>();
+        urlsToLogos.put("small","http://example.com/small");
+        urlsToLogos.put("medium","http://example.com/medium");
+        urlsToLogos.put("large","http://example.com/large");
+        final List<String> sortBysAvailable = new ArrayList<>();
+        sortBysAvailable.add("top");
+        sortBysAvailable.add("latest");
+        new Source("test","Test News", "Description", "http://example.com/test", "general", "en", "us", urlsToLogos, sortBysAvailable).save();
+        new Source("test2","Test News2", "Description", "http://example.com/test2", "music", "fr", "au", urlsToLogos, sortBysAvailable).save();
+        new Source("test3","Test News3", "Description", "http://example.com/test3", "science", "de", "it", urlsToLogos, sortBysAvailable).save();
+    }
+
+    @After
+    public void after() throws SQLException {
+        // Drop the sources table
+        DatabaseConnector.runQuery("DROP TABLE SOURCES;");
+    }
+
+    @Test
+    public void getSingleInvalidSource() throws SQLException {
+        final Optional<Source> source = Source.getSource("source");
+
+        // Make sure a source is not returned
+        assertThat(source.isPresent(), is(equalTo(false)));
+    }
+
+    @Test
+    public void getSingleValidSource() throws SQLException {
+        final Optional<Source> source = Source.getSource("test");
+
+        // Make sure a source is returned
+        assertThat(source.isPresent(), is(equalTo(true)));
+
+        // Make sure that the source properties are correct.
+        final Map<String, String> urlsToLogos = new HashMap<>();
+        urlsToLogos.put("small","http://example.com/small");
+        urlsToLogos.put("medium","http://example.com/medium");
+        urlsToLogos.put("large","http://example.com/large");
+
+        final List<String> sortBysAvailable = new ArrayList<>();
+        sortBysAvailable.add("top");
+        sortBysAvailable.add("latest");
+
+        assertThat(source.get().getId(), is(equalTo("test")));
+        assertThat(source.get().getName(), is(equalTo("Test News")));
+        assertThat(source.get().getDescription(), is(equalTo("Description")));
+        assertThat(source.get().getUrl(), is(equalTo("http://example.com/test")));
+        assertThat(source.get().getCategory(), is(equalTo("general")));
+        assertThat(source.get().getLanguage(), is(equalTo("en")));
+        assertThat(source.get().getCountry(), is(equalTo("us")));
+        assertThat(source.get().getUrlsToLogos(), is(equalTo(urlsToLogos)));
+        assertThat(source.get().getSortBysAvailable(), is(equalTo(sortBysAvailable)));
+    }
+
+    @Test
+    public void filterByCategory() throws SQLException {
+        // Make sure the category exists
+        Optional<Category> category = Category.getCategory("music");
+        assertThat(category.isPresent(), is(equalTo(true)));
+
+        // Get the sources matching the category
+        final List<Source> source = Source.getSources(category.get(), null, null);
+
+        // Make sure the correct sources are returned
+        assertThat(source.size(), is(equalTo(1)));
+
+        // Make sure that the source properties are correct.
+        final Map<String, String> urlsToLogos = new HashMap<>();
+        urlsToLogos.put("small","http://example.com/small");
+        urlsToLogos.put("medium","http://example.com/medium");
+        urlsToLogos.put("large","http://example.com/large");
+
+        final List<String> sortBysAvailable = new ArrayList<>();
+        sortBysAvailable.add("top");
+        sortBysAvailable.add("latest");
+
+        assertThat(source.get(0).getId(), is(equalTo("test2")));
+        assertThat(source.get(0).getName(), is(equalTo("Test News2")));
+        assertThat(source.get(0).getDescription(), is(equalTo("Description")));
+        assertThat(source.get(0).getUrl(), is(equalTo("http://example.com/test2")));
+        assertThat(source.get(0).getCategory(), is(equalTo("music")));
+        assertThat(source.get(0).getLanguage(), is(equalTo("fr")));
+        assertThat(source.get(0).getCountry(), is(equalTo("au")));
+        assertThat(source.get(0).getUrlsToLogos(), is(equalTo(urlsToLogos)));
+        assertThat(source.get(0).getSortBysAvailable(), is(equalTo(sortBysAvailable)));
+    }
+
+    @Test
+    public void filterByLanguage() throws SQLException {
+        // Make sure the language exists
+        Optional<Language> language = Language.getLanguage("de");
+        assertThat(language.isPresent(), is(equalTo(true)));
+
+        // Get the sources matching the language
+        final List<Source> source = Source.getSources(null, language.get(), null);
+
+        // Make sure the correct sources are returned
+        assertThat(source.size(), is(equalTo(1)));
+
+        // Make sure that the source properties are correct.
+        final Map<String, String> urlsToLogos = new HashMap<>();
+        urlsToLogos.put("small","http://example.com/small");
+        urlsToLogos.put("medium","http://example.com/medium");
+        urlsToLogos.put("large","http://example.com/large");
+
+        final List<String> sortBysAvailable = new ArrayList<>();
+        sortBysAvailable.add("top");
+        sortBysAvailable.add("latest");
+
+        assertThat(source.get(0).getId(), is(equalTo("test3")));
+        assertThat(source.get(0).getName(), is(equalTo("Test News3")));
+        assertThat(source.get(0).getDescription(), is(equalTo("Description")));
+        assertThat(source.get(0).getUrl(), is(equalTo("http://example.com/test3")));
+        assertThat(source.get(0).getCategory(), is(equalTo("science")));
+        assertThat(source.get(0).getLanguage(), is(equalTo("de")));
+        assertThat(source.get(0).getCountry(), is(equalTo("it")));
+        assertThat(source.get(0).getUrlsToLogos(), is(equalTo(urlsToLogos)));
+        assertThat(source.get(0).getSortBysAvailable(), is(equalTo(sortBysAvailable)));
+    }
+
+    @Test
+    public void filterByCountry() throws SQLException {
+        // Make sure the category exists
+        Optional<Country> country = Country.getCountry("us");
+        assertThat(country.isPresent(), is(equalTo(true)));
+
+        // Get the sources matching the category
+        final List<Source> source = Source.getSources(null, null, country.get());
+
+        // Make sure the correct sources are returned
+        assertThat(source.size(), is(equalTo(1)));
+
+        // Make sure that the source properties are correct.
+        final Map<String, String> urlsToLogos = new HashMap<>();
+        urlsToLogos.put("small","http://example.com/small");
+        urlsToLogos.put("medium","http://example.com/medium");
+        urlsToLogos.put("large","http://example.com/large");
+
+        final List<String> sortBysAvailable = new ArrayList<>();
+        sortBysAvailable.add("top");
+        sortBysAvailable.add("latest");
+
+        assertThat(source.get(0).getId(), is(equalTo("test")));
+        assertThat(source.get(0).getName(), is(equalTo("Test News")));
+        assertThat(source.get(0).getDescription(), is(equalTo("Description")));
+        assertThat(source.get(0).getUrl(), is(equalTo("http://example.com/test")));
+        assertThat(source.get(0).getCategory(), is(equalTo("general")));
+        assertThat(source.get(0).getLanguage(), is(equalTo("en")));
+        assertThat(source.get(0).getCountry(), is(equalTo("us")));
+        assertThat(source.get(0).getUrlsToLogos(), is(equalTo(urlsToLogos)));
+        assertThat(source.get(0).getSortBysAvailable(), is(equalTo(sortBysAvailable)));
+    }
+}
