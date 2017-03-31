d864679fff5720aaccfbff828a59782742a4d7b2
Mateusz Wiater <mateuszwiater@gmail.com>
2017-03-30 21:13:27 -0400

    Finished models.

--- hw2/src/main/java/com/mateuszwiater/csc435/model/Article.java
+++ hw2/src/main/java/com/mateuszwiater/csc435/model/Article.java
@@ -1,4 +1,98 @@
 package com.mateuszwiater.csc435.model;
 
+import com.mateuszwiater.csc435.db.DatabaseConnector;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Article {
+    private String id;
+    private final String author, title, description, url, urlToImage, source, publishedAt;
+
+    public Article(String author, String title, String description, String url, String urlToImage, String source, String publishedAt) {
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.source = source;
+        this.publishedAt = publishedAt;
+    }
+
+    public void save() throws SQLException {
+        if(id == null) {
+            final String query = String.format("INSERT INTO ARTICLES (AUTHOR,TITLE,DESCRIPTION,URL,URLTOIMAGE,SOURCE,PUBLISHEDAT) VALUES ('%s','%s','%s','%s','%s','%s','%s');",
+                    author, title, description, url, urlToImage, source, publishedAt);
+            DatabaseConnector.runQuery(query);
+        }
+    }
+
+    public static Optional<Article> getArticle(final String id) throws SQLException {
+        final String query = String.format("SELECT ID, AUTHOR, TITLE, DESCRIPTION, URL, URLTOIMAGE, SOURCE, PUBLISHEDAT FROM ARTICLES WHERE ID = '%s';",
+                id);
+
+        Optional<List<List<String>>> res = DatabaseConnector.runQuery(query);
+
+        if(res.isPresent()) {
+            return Optional.of(toArticle(res.get().get(0)));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static List<Article> getArticles(final Source source, final String sortBy) throws SQLException {
+        final String query = String.format("SELECT ID, AUTHOR, TITLE, DESCRIPTION, URL, URLTOIMAGE, SOURCE, PUBLISHEDAT FROM ARTICLES WHERE SOURCE = '%s' AND SORTBY = '%s';",
+                source, sortBy);
+
+        Optional<List<List<String>>> res = DatabaseConnector.runQuery(query);
+        final List<Article> articles = new ArrayList<>();
+
+        if(res.isPresent()) {
+            res.get().forEach(l -> articles.add(toArticle(l)));
+        }
+
+        return articles;
+    }
+
+    private static Article toArticle(final List<String> list) {
+        final Article article = new Article(list.get(1),list.get(2),list.get(3),list.get(4),list.get(5),list.get(6),list.get(7));
+        article.id = list.get(0);
+        return article;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+
 }
--- hw2/src/main/java/com/mateuszwiater/csc435/model/Preference.java
+++ hw2/src/main/java/com/mateuszwiater/csc435/model/Preference.java
@@ -1,4 +1,87 @@
 package com.mateuszwiater.csc435.model;
 
+import com.mateuszwiater.csc435.db.DatabaseConnector;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class Preference {
+    private final String apiKey, articleId;
+    private boolean favorite, read, isNew;
+
+    public Preference(final String apiKey, final String articleId, final boolean favorite, final boolean read) {
+        this.read = read;
+        this.isNew = true;
+        this.apiKey = apiKey;
+        this.favorite = favorite;
+        this.articleId = articleId;
+    }
+
+    public void save() throws SQLException {
+        if(isNew) {
+            final String query = String.format("INSERT INTO PREFERENCES (APIKEY,ARTICLEID,FAVORITE,READ) VALUES ('%s','%s','%s','%s');",
+                    apiKey, articleId, favorite, read);
+            DatabaseConnector.runQuery(query);
+        } else {
+            final String query = String.format("UPDATE PREFERENCES SET FAVORITE = '%s', READ = '%s' WHERE APIKEY = '%s' AND ARTICLEID = '%s';",
+                    favorite, read, apiKey, articleId);
+            DatabaseConnector.runQuery(query);
+        }
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public String getArticleId() {
+        return articleId;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public static Optional<Preference> getPreference(final String apiKey, final String articleId) throws SQLException {
+        final String query = String.format("SELECT APIKEY, ARTICLEID, FAVORITE, READ FROM PREFERENCES WHERE APIKEY = '%s' AND ARTICLEID = '%s';", apiKey, articleId);
+
+        final Optional<List<List<String>>> res = DatabaseConnector.runQuery(query);
+
+        if(res.isPresent()) {
+            return Optional.of(toPreference(res.get().get(0)));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<List<Preference>> getPreferences(final String apiKey) throws SQLException {
+        final String query = String.format("SELECT APIKEY, ARTICLEID, FAVORITE, READ FROM PREFERENCES WHERE APIKEY = '%s';", apiKey);
+
+        final Optional<List<List<String>>> res = DatabaseConnector.runQuery(query);
+
+        if(res.isPresent()) {
+            return Optional.of(res.get().stream().map(Preference::toPreference).collect(Collectors.toList()));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private static Preference toPreference(final List<String> list) {
+        final Preference preference = new Preference(list.get(0), list.get(1), Boolean.valueOf(list.get(2)), Boolean.valueOf(list.get(3)));
+        preference.isNew = false;
+        return preference;
+    }
 }
